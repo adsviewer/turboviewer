5bf6960a472cf57af29b00354db4cd7522698bed
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2024-03-17 09:09:54 +0200

    Deploy with vercel analytics enabled

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import React from 'react';
+import { Analytics as _dontDelete } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
