09002b54c4f6910aa69ee1eca3eaa06571c5c6ce
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2024-04-08 20:04:15 +0300

    Server now serves the correct integration status

--- apps/server/src/schema/integrations/integration-operations.ts
+++ apps/server/src/schema/integrations/integration-operations.ts
@@ -1,25 +1,32 @@
-import { IntegrationTypeEnum } from '@repo/database';
+import { type Integration, IntegrationTypeEnum, prisma } from '@repo/database';
 import { builder } from '../builder';
 import { IntegrationListItemDto, IntegrationStatus } from './integration-types';
 
 builder.queryFields((t) => ({
   integrations: t.withAuth({ authenticated: true }).field({
     type: [IntegrationListItemDto],
-    resolve: (_root, _args, _ctx, _info) => {
-      return [
-        {
-          type: IntegrationTypeEnum.FACEBOOK,
-          status: IntegrationStatus.ComingSoon,
+    resolve: async (_root, _args, ctx, _info) => {
+      const integrations = await prisma.integration.findMany({
+        where: {
+          organizationId: ctx.organizationId,
         },
-        {
-          type: IntegrationTypeEnum.LINKEDIN,
-          status: IntegrationStatus.ComingSoon,
-        },
-        {
-          type: IntegrationTypeEnum.TIKTOK,
-          status: IntegrationStatus.ComingSoon,
-        },
-      ];
+      });
+
+      return Object.values(IntegrationTypeEnum).map((value) => ({
+        type: value,
+        status: integrationStatus(value, integrations),
+      }));
     },
   }),
 }));
+
+const integrationStatus = (type: IntegrationTypeEnum, integrations: Integration[]): IntegrationStatus => {
+  const SUPPORTED_INTEGRATIONS: IntegrationTypeEnum[] = [];
+  if (!SUPPORTED_INTEGRATIONS.includes(type)) return IntegrationStatus.ComingSoon;
+
+  const integration = integrations.find((i) => i.type === type);
+  if (!integration) return IntegrationStatus.NotConnected;
+  if (integration.refreshTokenExpiresAt && integration.refreshTokenExpiresAt < new Date())
+    return IntegrationStatus.Expired;
+  return IntegrationStatus.Connected;
+};
