50336717d2e20ec8b9e49ade0f059694e01f4a99
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2024-05-11 16:45:42 +0300

    don't use neon locally

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -9,8 +9,10 @@ neonConfig.webSocketConstructor = ws;
 const connectionString = env.DATABASE_URL;
 
 const pool = new Pool({ connectionString });
+const params = new URLSearchParams(connectionString);
+const hasPgbouncer = params.has('pgbouncer');
 const adapter = new PrismaNeon(pool);
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient(hasPgbouncer ? { adapter } : undefined);
 
 // eslint-disable-next-line import/no-relative-packages -- we need to import from the generated client
 export * from '../.prisma';
