2bd5f08a353232733abfcd3867fc18f4c242eb51
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2024-05-18 09:12:36 +0300

    remove pid, hostname
    * use json structure in lambda
    * use non pretty format in prod

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,5 +1,5 @@
 import { type LoggerOptions, pino } from 'pino';
-import { pinoLambdaDestination } from 'pino-lambda';
+import { type LogData, pinoLambdaDestination, PinoLogFormatter } from 'pino-lambda';
 
 export interface SlackTransportOptions {
   webhookUrl: string;
@@ -7,16 +7,30 @@ export interface SlackTransportOptions {
 
 const options: Partial<SlackTransportOptions> = { webhookUrl: process.env.SLACK_WEBHOOK_URL };
 const optionsOrStream: LoggerOptions = {
+  base: undefined,
   transport: {
     targets: [
       ...(options.webhookUrl ? [{ level: 'error', target: './slack.mjs', options }] : []),
-      { target: 'pino-pretty', options: { destination: 1 } },
+      { target: process.env.MODE ? 'pino/file' : 'pino-pretty', options: { destination: 1 } },
     ],
   },
 };
+
+class CustomLogFormatter extends PinoLogFormatter {
+  format(data: LogData & { pid?: string; hostname?: string }): string {
+    delete data.pid;
+    delete data.hostname;
+    return super.format(data);
+  }
+}
+
 export const logger =
   process.env.IS_LAMBDA === 'true'
-    ? pino(pinoLambdaDestination())
+    ? pino(
+        pinoLambdaDestination({
+          formatter: new CustomLogFormatter(),
+        }),
+      )
     : pino(process.env.NEXT_RUNTIME ? {} : optionsOrStream);
 
 export { lambdaRequestTracker } from 'pino-lambda';
