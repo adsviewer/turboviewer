200796dc4cd4a293ee38ddc242fd3f7e9d44f0a4
Lefteris Hytiroglou <lehy64@gmail.com>
2024-05-19 11:00:55 +0300

    integrations

--- apps/web2/src/app/(authenticated)/integrations/components/integration-card.tsx
+++ apps/web2/src/app/(authenticated)/integrations/components/integration-card.tsx
@@ -1,38 +1,70 @@
 'use client';
 
-import { Badge, Button, Card, Group, Image, Text, useMantineTheme } from '@mantine/core';
+import { Badge, Button, Card, Flex, Group, Text, useMantineTheme } from '@mantine/core';
 import { type ReactNode } from 'react';
+import { type IntegrationType } from '@/graphql/generated/schema-server';
+// import { deAuthIntegration } from '../actions';
 
 interface IntegrationProps {
   title: string;
   description: string;
+  integrationType: IntegrationType;
   isConnected: boolean;
+  isAvailable: boolean;
+  image?: ReactNode;
 }
 
 export default function IntegrationCard(props: IntegrationProps): ReactNode {
   const theme = useMantineTheme();
+
+  const handleRevoke = (): void => {
+    // void deAuthIntegration(props.integrationType).then((res) => {
+    //   // logger.info(res, 'NEW RES!');
+    // });
+  };
+
+  const renderIntegrationButton = (): ReactNode => {
+    if (props.isAvailable) {
+      if (!props.isConnected) {
+        return <Button mt="lg">Connect</Button>;
+      }
+      return (
+        <Button
+          mt="lg"
+          color={theme.colors.red[7]}
+          onClick={() => {
+            handleRevoke();
+          }}
+        >
+          Revoke
+        </Button>
+      );
+    }
+    return (
+      <Button mt="lg" disabled>
+        Available soon!
+      </Button>
+    );
+  };
+
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
       <Card.Section>
-        <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-6.png" height={300} />
+        <Flex justify="center" align="center" p="xl">
+          {props.image ? props.image : null}
+        </Flex>
       </Card.Section>
 
       <Group justify="space-between" mt="md" mb="xs">
         <Text fw={500}>{props.title}</Text>
-        {props.isConnected ? <Badge color="green">Connected</Badge> : null}
+        {props.isConnected ? <Badge color="green">Connected</Badge> : <Badge color="gray">Not Connected</Badge>}
       </Group>
 
-      <Text size="sm" c="dimmed">
+      <Text size="sm" c="dimmed" mb="auto">
         {props.description}
       </Text>
 
-      {!props.isConnected ? (
-        <Button mt="lg">Connect</Button>
-      ) : (
-        <Button mt="lg" color={theme.colors.red[7]}>
-          Revoke
-        </Button>
-      )}
+      {renderIntegrationButton()}
     </Card>
   );
 }
--- apps/web2/src/app/(authenticated)/integrations/components/integrations-grid.tsx
+++ apps/web2/src/app/(authenticated)/integrations/components/integrations-grid.tsx
@@ -1,14 +1,57 @@
+import Image from 'next/image';
 import { SimpleGrid } from '@mantine/core';
 import { type ReactNode } from 'react';
+import metaLogo from '@/assets/images/meta-logo.png';
+import tiktokLogo from '@/assets/images/tiktok-logo.png';
+import linkedinLogo from '@/assets/images/linkedin-logo.png';
+import { IntegrationStatus, IntegrationType, type SettingsChannelsQuery } from '@/graphql/generated/schema-server';
 import IntegrationCard from './integration-card';
 
-export default function IntegrationsGrid(): ReactNode {
+interface IntegrationProps {
+  integrations: SettingsChannelsQuery['settingsChannels'];
+}
+
+export default function IntegrationsGrid(props: IntegrationProps): ReactNode {
+  // (backend response needs refactor to return object with IntegrationType as key)
+  const isIntegrationAvailable = (status: IntegrationStatus): boolean => {
+    if (status !== IntegrationStatus.ComingSoon) {
+      return true;
+    }
+    return false;
+  };
+
+  const isIntegrationConnected = (status: IntegrationStatus): boolean => {
+    if (status === IntegrationStatus.Connected) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }}>
       <IntegrationCard
         title="Meta"
-        description="Connect your Meta account and manage advertisments for every Meta Platforms application!"
-        isConnected={false}
+        description="Connect your Meta account and view analytics for your ad campaigns on every Meta Platforms application!"
+        integrationType={IntegrationType.META}
+        isConnected={isIntegrationConnected(props.integrations[0].status)}
+        isAvailable={isIntegrationAvailable(props.integrations[0].status)}
+        image={<Image src={metaLogo} alt="Meta" width={300} priority />}
+      />
+      <IntegrationCard
+        title="TikTok"
+        description="Connect your TikTok account and manage all your TikTok advertisments!"
+        integrationType={IntegrationType.TIKTOK}
+        isConnected={isIntegrationConnected(props.integrations[1].status)}
+        isAvailable={isIntegrationAvailable(props.integrations[1].status)}
+        image={<Image src={tiktokLogo} alt="TikTok" width={300} priority />}
+      />
+      <IntegrationCard
+        title="LinkedIn"
+        description="Connect your LinkedIn account and manage all your LinkedIn advertisments!"
+        integrationType={IntegrationType.LINKEDIN}
+        isConnected={isIntegrationConnected(props.integrations[2].status)}
+        isAvailable={isIntegrationAvailable(props.integrations[2].status)}
+        image={<Image src={linkedinLogo} alt="LinkedIn" width={300} priority />}
       />
     </SimpleGrid>
   );
