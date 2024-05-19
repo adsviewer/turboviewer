c3a02fb0e0a4fc947013be0635038d5c6b51b947
Lefteris Hytiroglou <lehy64@gmail.com>
2024-05-19 11:00:55 +0300

    dark theme toggle & language picker layout

---
+++ apps/web2/src/components/buttons/language-picker/language-picker.tsx
@@ -0,0 +1,55 @@
+import { type ReactNode, useState } from 'react';
+import { UnstyledButton, Menu, Image, Group } from '@mantine/core';
+import { IconChevronDown } from '@tabler/icons-react';
+import images from './images';
+import classes from './language-picker.module.scss';
+
+const data = [
+  { label: 'English', image: images.english },
+  { label: 'German', image: images.german },
+  { label: 'Italian', image: images.italian },
+  { label: 'French', image: images.french },
+  { label: 'Polish', image: images.polish },
+  { label: 'Greek', image: images.greek },
+];
+
+export default function LanguagePicker(): ReactNode {
+  const [opened, setOpened] = useState(false);
+  const [selected, setSelected] = useState(data[0]);
+  const items = data.map((item) => (
+    <Menu.Item
+      leftSection={<Image src={item.image} width={18} height={18} />}
+      onClick={() => {
+        setSelected(item);
+      }}
+      key={item.label}
+    >
+      {item.label}
+    </Menu.Item>
+  ));
+
+  return (
+    <Menu
+      onOpen={() => {
+        setOpened(true);
+      }}
+      onClose={() => {
+        setOpened(false);
+      }}
+      radius="md"
+      width="target"
+      withinPortal
+    >
+      <Menu.Target>
+        <UnstyledButton className={classes.control} data-expanded={opened || undefined}>
+          <Group gap="xs">
+            <Image src={selected.image} width={22} height={22} />
+            <span className={classes.label}>{selected.label}</span>
+          </Group>
+          <IconChevronDown size="1rem" className={classes.icon} stroke={1.5} />
+        </UnstyledButton>
+      </Menu.Target>
+      <Menu.Dropdown>{items}</Menu.Dropdown>
+    </Menu>
+  );
+}
