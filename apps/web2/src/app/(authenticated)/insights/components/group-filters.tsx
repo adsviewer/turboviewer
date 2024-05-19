64df40c730d5090b173033b1d0788e567952c613
Lefteris Hytiroglou <lehy64@gmail.com>
2024-05-19 11:00:55 +0300

    insights filtering layout

---
+++ apps/web2/src/app/(authenticated)/insights/components/group-filters.tsx
@@ -0,0 +1,55 @@
+import { Checkbox, Flex, MultiSelect, Text } from '@mantine/core';
+import { type ReactNode } from 'react';
+
+export default function GroupFilters(): ReactNode {
+  return (
+    <Flex direction="column">
+      <Text size="xl">Filters</Text>
+      <Text size="sm" mt="xs">
+        Accounts
+      </Text>
+      <MultiSelect
+        placeholder="Select accounts..."
+        data={['Some Dude 1', 'Some Dude 2', 'Some Dude 3', 'Some Dude 4']}
+        comboboxProps={{ transitionProps: { transition: 'fade-down', duration: 200 } }}
+        my={4}
+      />
+      <Text size="sm" mt="xs">
+        Positions
+      </Text>
+      <MultiSelect
+        placeholder="Select positions..."
+        data={['Facebook Reels', 'Facebook Stories', 'Instagram Reels', 'Instagram Stories']}
+        comboboxProps={{ transitionProps: { transition: 'fade-down', duration: 200 } }}
+        my={4}
+      />
+      <Text size="sm" mt="xs">
+        Devices
+      </Text>
+      <MultiSelect
+        placeholder="Select devices..."
+        data={['Mobile (Web)', 'Mobile (App)', 'Desktop', 'Unknown']}
+        comboboxProps={{ transitionProps: { transition: 'fade-down', duration: 200 } }}
+        my={4}
+      />
+      <Text size="sm" mt="xs">
+        Publishers
+      </Text>
+      <MultiSelect
+        placeholder="Select publishers..."
+        data={['Facebook', 'Instagram', 'Messenger', 'Audience Network', 'LinkedIn', 'TikTok', 'Unknown']}
+        comboboxProps={{ transitionProps: { transition: 'fade-down', duration: 200 } }}
+        my={4}
+      />
+      <Text size="sm" mt="lg">
+        Group By
+      </Text>
+      <Checkbox label="Account" my={4} />
+      <Checkbox label="Ad ID" my={4} />
+      <Checkbox label="Device" my={4} />
+      <Checkbox label="Date" my={4} />
+      <Checkbox label="Publisher" my={4} />
+      <Checkbox label="Position" my={4} />
+    </Flex>
+  );
+}
