1eb20c03a16afdf0d24006ed862929ea8835608e
Lefteris Hytiroglou <lehy64@gmail.com>
2024-05-19 11:00:55 +0300

    dark theme toggle & language picker layout

--- apps/web2/src/components/shells/main-shell/main-shell.tsx
+++ apps/web2/src/components/shells/main-shell/main-shell.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { AppShell, AppShellHeader, AppShellMain, AppShellNavbar, Burger, Group, NavLink } from '@mantine/core';
+import { AppShell, AppShellHeader, AppShellMain, AppShellNavbar, Burger, Flex, Group, NavLink } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { type ReactNode } from 'react';
 import { IconGraph, IconSettings } from '@tabler/icons-react';
 import { usePathname } from 'next/navigation';
 import { LogoFull } from '@/components/misc/logo-full';
+import ColorSchemeToggle from '@/components/buttons/color-scheme-toggle';
+import LanguagePicker from '@/components/buttons/language-picker/language-picker';
 
 export function MainAppShell({ children }: { children: React.ReactNode }): ReactNode {
   const [opened, { toggle }] = useDisclosure();
@@ -21,6 +23,10 @@ export function MainAppShell({ children }: { children: React.ReactNode }): React
         <Group h="100%" px="md">
           <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
           <LogoFull />
+          <Flex align="center" justify="flex-end" ml="auto" gap="sm">
+            <LanguagePicker />
+            <ColorSchemeToggle />
+          </Flex>
         </Group>
       </AppShellHeader>
       <AppShellNavbar p="md">
