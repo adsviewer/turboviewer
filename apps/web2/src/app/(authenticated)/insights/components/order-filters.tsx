d14ba670dc594e4ba0332edfa0c646769372fbf7
Lefteris Hytiroglou <lehy64@gmail.com>
2024-05-19 11:00:55 +0300

    responsive order filters

---
+++ apps/web2/src/app/(authenticated)/insights/components/order-filters.tsx
@@ -0,0 +1,50 @@
+import { type ReactNode } from 'react';
+import { Flex, Text, Select } from '@mantine/core';
+
+export default function OrderFilters(): ReactNode {
+  return (
+    <Flex w="100%" mb="lg" align="center" wrap="wrap">
+      {/* Page data info */}
+      <Text size="md" mr="auto">
+        Page 1 of 1 (10 results in total)
+      </Text>
+
+      {/* Filters */}
+      <Flex align="center" mr="sm" my="md">
+        {/* Page size filter */}
+        <Text size="md" mr="sm">
+          Page size:
+        </Text>
+        <Select
+          placeholder="Pick value"
+          data={['6', '12', '18', '50', '100']}
+          defaultValue="12"
+          allowDeselect={false}
+          maw={90}
+        />
+      </Flex>
+
+      <Flex align="center">
+        {/* Order filter */}
+        <Text size="md" mr="sm">
+          Order by:
+        </Text>
+        <Select
+          placeholder="Pick value"
+          data={['Spent', 'Impressions']}
+          defaultValue="Spent"
+          allowDeselect={false}
+          maw={150}
+          mr="sm"
+        />
+        <Select
+          placeholder="Pick value"
+          data={['Ascending', 'Descending']}
+          defaultValue="Descending"
+          allowDeselect={false}
+          maw={150}
+        />
+      </Flex>
+    </Flex>
+  );
+}
--- apps/web2/src/app/(authenticated)/insights/page.tsx
+++ apps/web2/src/app/(authenticated)/insights/page.tsx
@@ -1,50 +1,12 @@
 import { type ReactNode } from 'react';
-import { Flex, Text, Select } from '@mantine/core';
 import InsightsGrid from './components/insights-grid';
+import OrderFilters from './components/order-filters';
 
 export default function Insights(): ReactNode {
   return (
     <>
       <h1>Insights</h1>
-      <Flex w="100%" mb="lg" align="center">
-        {/* Page data info */}
-        <Text size="md">Page 1 of 1 (10 results in total)</Text>
-
-        {/* Filters */}
-        <Flex ml="auto" align="center" mr="sm">
-          {/* Page size filter */}
-          <Text size="md" mr="sm">
-            Page size:
-          </Text>
-          <Select
-            placeholder="Pick value"
-            data={['6', '12', '18', '50', '100']}
-            defaultValue="12"
-            allowDeselect={false}
-            maw={90}
-          />
-        </Flex>
-
-        {/* Order filter */}
-        <Text size="md" mr="sm">
-          Order by:
-        </Text>
-        <Select
-          placeholder="Pick value"
-          data={['Spent', 'Impressions']}
-          defaultValue="Spent"
-          allowDeselect={false}
-          maw={150}
-          mr="sm"
-        />
-        <Select
-          placeholder="Pick value"
-          data={['Ascending', 'Descending']}
-          defaultValue="Descending"
-          allowDeselect={false}
-          maw={150}
-        />
-      </Flex>
+      <OrderFilters />
       <InsightsGrid />
     </>
   );
