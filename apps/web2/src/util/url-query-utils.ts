d05321c586e3a94035956b9556e9b73f436142d9
Lefteris Hytiroglou <lehy64@gmail.com>
2024-05-21 01:07:25 +0300

    mad url utils refactor & filter checkboxes fix

--- apps/web2/src/util/url-query-utils.ts
+++ apps/web2/src/util/url-query-utils.ts
@@ -1,6 +1,6 @@
 import { type ReadonlyURLSearchParams } from 'next/navigation';
 
-const groupedByKey = 'groupedBy';
+export const groupedByKey = 'groupedBy';
 
 export interface QueryParamsType {
   key: string;
@@ -13,80 +13,6 @@ export const userActionOverrideParams: QueryParamsType = {
   key: 'overrideFilters',
   value: 'true',
 };
-
-export const createQueryParamString = (searchParams: ReadonlyURLSearchParams, key: string, value: string): string => {
-  if (!isParamInSearchParams(searchParams, key, value)) {
-    const params = new URLSearchParams(searchParams.toString());
-    params.set(key, value);
-
-    // We should always remove the page parameter when any of the search params change
-    // (so that results are return beginning from page 1 again)
-    params.delete('page');
-    return params.toString();
-  }
-  return 'error';
-};
-
-export const createURLWithNewParam = (
-  pathname: string,
-  currentParams: ReadonlyURLSearchParams,
-  key: string,
-  value: string,
-): string => {
-  const queryParams = createQueryParamString(currentParams, key, value);
-  if (currentParams.size === 0) {
-    return `${pathname}/?${queryParams}`;
-  }
-  return `${pathname}?${currentParams.toString()}&${queryParams}`;
-};
-
-export const createURLWithRemovedParam = (
-  pathname: string,
-  currentParams: ReadonlyURLSearchParams,
-  key: string,
-  value: string,
-): string => {
-  const paramString = currentParams.toString();
-  let newURL = `${pathname}?${paramString}`;
-
-  const paramToRemove = `${key}=${value}`;
-
-  if (paramString.includes(`&${paramToRemove}`)) {
-    newURL = newURL.replace(`&${paramToRemove}`, '');
-  } else if (paramString.includes(paramToRemove)) {
-    newURL = newURL.replace(paramToRemove, '');
-
-    // Handle the case where the first parameter is removed
-    if (newURL.endsWith('?')) {
-      newURL = newURL.slice(0, -1);
-    } else {
-      newURL = newURL.replace('?&', '?');
-    }
-  } else {
-    return 'error=RemovedParamNotFound';
-  }
-
-  return newURL;
-};
-
-// Replaces the value of a query param's key of a URL
-// export const createURLWithReplacedParam = (
-//   pathname: string,
-//   searchParams: ReadonlyURLSearchParams,
-//   key: string,
-//   newValue: string,
-// ): string => {
-//   const newParams = new URLSearchParams(searchParams.toString());
-//   const newURL = `${pathname}?${paramString}`;
-
-//   const regex = new RegExp(`([?&])(${key}=)([^&]*)`, 'i');
-
-//   if (newURL.match(regex)) {
-//     return newURL.replace(regex, `$1${key}=${newValue}`);
-//   }
-//   const separator = newURL.includes('?') ? '&' : '?';
-//   return `${newURL}${separator}${key}=${newValue}`;
-// };
 
 // If the specific key exists its value is replaced, otherwise
 // it's added as a new key=value param
@@ -101,10 +27,12 @@ export const addOrReplaceURLParams = (
   // Specific case for groupBy (insights) so that it handles
   // multiple params with the same key & different values
   if (key === groupedByKey) {
+    // If it doesn't exist, just add it
     if (!newParams.has(groupedByKey, newValue)) {
-      newParams.set(groupedByKey, newValue);
+      newParams.append(groupedByKey, newValue);
       return `${pathname}?${newParams.toString()}`;
     }
+    // If it already exists, remove it!
     newParams.delete(groupedByKey, newValue);
     return `${pathname}?${newParams.toString()}`;
   }
