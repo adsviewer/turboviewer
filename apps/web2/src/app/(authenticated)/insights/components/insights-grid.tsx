08d3153773d77d6e3280742245681d00b72aa83d
Lefteris Hytiroglou <lehy64@gmail.com>
2024-05-24 14:25:35 +0300

    date format & conditional rendering

--- apps/web2/src/app/(authenticated)/insights/components/insights-grid.tsx
+++ apps/web2/src/app/(authenticated)/insights/components/insights-grid.tsx
@@ -1,6 +1,6 @@
 import { SimpleGrid } from '@mantine/core';
 import { type Key, type ReactNode } from 'react';
-import { useFormatter } from 'next-intl';
+import { type DateTimeFormatOptions, useFormatter } from 'next-intl';
 import { type InsightsQuery } from '@/graphql/generated/schema-server';
 import InsightCard from './insight-card';
 
@@ -10,6 +10,10 @@ interface PropsType {
 
 export default function InsightsGrid(props: PropsType): ReactNode {
   const format = useFormatter();
+  const dateFormatOptions: DateTimeFormatOptions = {
+    month: 'numeric',
+    day: 'numeric',
+  };
 
   return (
     <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }} style={{ display: 'relative' }}>
@@ -24,6 +28,7 @@ export default function InsightsGrid(props: PropsType): ReactNode {
               amountSpent={format.number(insight.spend, { style: 'currency', currency: insight.currency ?? 'EUR' })}
               currency={insight.currency}
               impressions={insight.impressions}
+              date={insight.date ? format.dateTime(new Date(insight.date), dateFormatOptions) : null}
             />
           ))
         : null}
