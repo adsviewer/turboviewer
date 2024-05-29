4146d51309ee730d3110f1fb73068506779035df
elevchyt <lehy64@gmail.com>
2024-05-29 12:02:38 +0300

    update language & base page controls

--- apps/web2/src/app/(authenticated)/insights/components/insights-grid.tsx
+++ apps/web2/src/app/(authenticated)/insights/components/insights-grid.tsx
@@ -1,7 +1,8 @@
-import { SimpleGrid } from '@mantine/core';
+import { Flex, SimpleGrid } from '@mantine/core';
 import { type Key, type ReactNode } from 'react';
 import { type InsightsQuery } from '@/graphql/generated/schema-server';
 import InsightCard from './insight-card';
+import PageControls from './page-controls';
 
 interface PropsType {
   insights: InsightsQuery['insights']['edges'];
@@ -9,21 +10,24 @@ interface PropsType {
 
 export default function InsightsGrid(props: PropsType): ReactNode {
   return (
-    <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }} style={{ display: 'relative' }}>
-      {props.insights.length
-        ? props.insights.map((insight) => (
-            <InsightCard
-              key={insight.id as Key}
-              title={insight.publisher}
-              description={insight.position}
-              device={insight.device}
-              rank="good"
-              currency={insight.currency}
-              datapoints={insight.datapoints}
-              iframe={insight.iFrame}
-            />
-          ))
-        : null}
-    </SimpleGrid>
+    <Flex direction="column">
+      <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }} style={{ display: 'relative' }}>
+        {props.insights.length
+          ? props.insights.map((insight) => (
+              <InsightCard
+                key={insight.id as Key}
+                title={insight.publisher}
+                description={insight.position}
+                device={insight.device}
+                rank="good"
+                currency={insight.currency}
+                datapoints={insight.datapoints}
+                iframe={insight.iFrame}
+              />
+            ))
+          : null}
+      </SimpleGrid>
+      <PageControls />
+    </Flex>
   );
 }
