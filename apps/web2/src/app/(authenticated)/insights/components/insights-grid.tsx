6877091b194f604a61614043047fe3aaa8f35bd0
elevchyt <lehy64@gmail.com>
2024-05-31 11:25:04 +0300

    smooth transitions

--- apps/web2/src/app/(authenticated)/insights/components/insights-grid.tsx
+++ apps/web2/src/app/(authenticated)/insights/components/insights-grid.tsx
@@ -1,5 +1,7 @@
-import { SimpleGrid } from '@mantine/core';
-import { type Key, type ReactNode } from 'react';
+'use client';
+
+import { Text, SimpleGrid, Transition } from '@mantine/core';
+import { useEffect, useState, type Key, type ReactNode } from 'react';
 import { type InsightsQuery } from '@/graphql/generated/schema-server';
 import InsightCard from './insight-card';
 
@@ -8,22 +10,40 @@ interface PropsType {
 }
 
 export default function InsightsGrid(props: PropsType): ReactNode {
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  // Refresh animation on insights data changes
+  useEffect(() => {
+    setIsMounted(false);
+    setTimeout(() => {
+      setIsMounted(true);
+    }, 0);
+  }, [props.insights]);
+
   return (
-    <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }} style={{ display: 'relative' }}>
-      {props.insights.length
-        ? props.insights.map((insight, index) => (
-            <InsightCard
-              key={index as Key}
-              title={insight.publisher}
-              description={insight.position}
-              device={insight.device}
-              rank="good"
-              currency={insight.currency}
-              datapoints={insight.datapoints}
-              iframe={insight.iFrame}
-            />
-          ))
-        : null}
-    </SimpleGrid>
+    <Transition mounted={isMounted} transition="fade" duration={400} timingFunction="ease">
+      {(styles) => (
+        <div style={styles}>
+          <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }} style={{ display: 'relative' }}>
+            {props.insights.length ? (
+              props.insights.map((insight, index) => (
+                <InsightCard
+                  key={index as Key}
+                  title={insight.publisher}
+                  description={insight.position}
+                  device={insight.device}
+                  rank="good"
+                  currency={insight.currency}
+                  datapoints={insight.datapoints}
+                  iframe={insight.iFrame}
+                />
+              ))
+            ) : (
+              <Text>No results found.</Text>
+            )}
+          </SimpleGrid>
+        </div>
+      )}
+    </Transition>
   );
 }
