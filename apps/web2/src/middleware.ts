0e8ff216e1a535c869e2e8772fad9491959d9204
elevchyt <lehy64@gmail.com>
2024-06-04 08:37:55 +0300

    middleware matcher change to use recommended setting

--- apps/web2/src/middleware.ts
+++ apps/web2/src/middleware.ts
@@ -20,14 +20,27 @@ const publicPaths = [
 ];
 
 // Stop Middleware running on static files and public folder
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - _next (static files & image optimization files)
+//      * - favicon.ico (favicon file)
+//      */
+//     '/((?!_next|favicon.ico).*)',
+//   ],
+// };
+
 export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - _next (static files & image optimization files)
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!_next|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 };
 
