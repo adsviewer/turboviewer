d6957942a1b3e6a2485f8d1bfd74273dc74c72a7
elevchyt <lehy64@gmail.com>
2024-06-05 12:22:14 +0300

    lang mismatch fix

--- apps/web2/src/app/layout.tsx
+++ apps/web2/src/app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }): React.ReactNode {
   return (
-    <html lang="en">
+    <html lang="en-gb">
       <body>{children}</body>
     </html>
   );
