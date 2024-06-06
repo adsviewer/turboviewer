b2418e3e455ca010f17d5206ac26f57527211e0b
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-06-06 12:58:51 +0300

    remove eslint in package.json

--- packages/config-eslint/next.cjs
+++ packages/config-eslint/next.cjs
@@ -1,6 +1,6 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path');
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json');
 
 /*
  * This is a custom ESLint configuration for use with
@@ -13,12 +13,14 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 module.exports = {
   extends: [
-    "@vercel/style-guide/eslint/node",
-    "@vercel/style-guide/eslint/typescript",
-    "@vercel/style-guide/eslint/browser",
-    "@vercel/style-guide/eslint/react",
-    "@vercel/style-guide/eslint/next",
-    "eslint-config-turbo",
+    '@vercel/style-guide/eslint/node',
+    '@vercel/style-guide/eslint/typescript',
+    '@vercel/style-guide/eslint/browser',
+    '@vercel/style-guide/eslint/react',
+    '@vercel/style-guide/eslint/next',
+    // Turborepo custom eslint configuration configures the following rules:
+    //  - https://github.com/vercel/turbo/blob/main/packages/eslint-plugin-turbo/docs/rules/no-undeclared-env-vars.md
+    'eslint-config-turbo',
   ].map(require.resolve),
   parserOptions: {
     project,
@@ -28,18 +30,15 @@ module.exports = {
     JSX: true,
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project,
       },
-      node: {
-        extensions: [".mjs", ".js", ".jsx", ".ts", ".tsx"],
-      },
     },
   },
-  ignorePatterns: ["node_modules/", "dist/", "src/graphql/generated/*.ts"],
+  ignorePatterns: ['node_modules/', 'dist/', 'src/graphql/generated/*.ts'],
   // add rules configurations here
   rules: {
-    "import/no-default-export": "off",
+    'import/no-default-export': 'off',
   },
 };
