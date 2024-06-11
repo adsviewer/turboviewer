ec8beaeb026ce1ef084117bd9b776f523a741d03
elevchyt <lehy64@gmail.com>
2024-06-11 18:12:13 +0300

    suspense on unauth layout

--- apps/web/src/app/(unauthenticated)/layout.tsx
+++ apps/web/src/app/(unauthenticated)/layout.tsx
@@ -1,3 +1,6 @@
+import { Suspense } from 'react';
+import LoaderCentered from '@/components/misc/loader-centered';
+
 export default function RootLayout({ children }: { children: React.ReactNode }): React.ReactNode {
-  return <>{children}</>;
+  return <Suspense fallback={<LoaderCentered />}>{children}</Suspense>;
 }
