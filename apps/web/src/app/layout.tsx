298cba6a265c22cd848f055d307ebc2cf1413e00
elevchyt <lehy64@gmail.com>
2024-06-11 18:33:14 +0300

    user select disable on non-inputs

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
   return (
     <html lang={locale}>
-      <body>
+      <body style={{ userSelect: 'none' }}>
         <ColorSchemeScript />
         <NextIntlClientProvider messages={messages}>
           <MantineProvider>{children}</MantineProvider>
