6940108247f52df098eb0199bb98b74a42b3db8c
elevchyt <lehy64@gmail.com>
2024-06-27 23:04:35 +0300

    return to sign in page option

--- apps/web/src/app/(unauthenticated)/confirm-email/page.tsx
+++ apps/web/src/app/(unauthenticated)/confirm-email/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { Button, Flex, Text } from '@mantine/core';
 import { useTranslations } from 'next-intl';
 import { resendEmailConfirmation } from '@/app/(unauthenticated)/confirm-email/actions';
+import { signOut } from '../actions';
 
 export default function ConfirmEmail(): React.ReactNode {
   const t = useTranslations('authentication');
@@ -50,6 +51,18 @@ export default function ConfirmEmail(): React.ReactNode {
           ? ` (${String(Math.floor(resendEmailTime / 60))}:${String(resendEmailTime % 60).padStart(2, '0')})`
           : null}
       </Button>
+      <Text
+        onClick={() => {
+          signOut();
+        }}
+        td="underline"
+        size="xs"
+        c="dimmed"
+        my="md"
+        style={{ cursor: 'pointer' }}
+      >
+        {t('returnToSignIn')}
+      </Text>
     </Flex>
   );
 }
