1726140b1fe7f899fedd64ac12d35b4138440625
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-06-27 14:14:16 +0300

    add sentry sdk in nextjs

--- apps/server/src/instrument.ts
+++ apps/server/src/instrument.ts
@@ -1,9 +1,11 @@
 import * as Sentry from '@sentry/node';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
+import { MODE } from '@repo/utils';
 
 Sentry.init({
   dsn: 'https://b3719092a80366bd037eb7b5adfe15ff@o4507502891040768.ingest.de.sentry.io/4507502899560528',
-  integrations: [nodeProfilingIntegration()],
+  environment: MODE,
+  integrations: [nodeProfilingIntegration(), Sentry.prismaIntegration()],
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
 
---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://a4e338501accee2adf9e58d71b25367e@o4507502891040768.ingest.de.sentry.io/4507503305949264',
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+});
