1da8a47e35d35e8f125e6e277c0a19c7cb429d54
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-06-27 12:22:28 +0300

    add sentry sdk in server

---
+++ apps/server/src/instrument.ts
@@ -0,0 +1,12 @@
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+
+Sentry.init({
+  dsn: 'https://b3719092a80366bd037eb7b5adfe15ff@o4507502891040768.ingest.de.sentry.io/4507502899560528',
+  integrations: [nodeProfilingIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
