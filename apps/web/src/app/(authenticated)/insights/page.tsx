e03992526131ecf76511bf07d020b914af25dc76
elevchyt <lehy64@gmail.com>
2024-07-03 23:46:05 +0300

    loading overlay & global loading state

--- apps/web/src/app/(authenticated)/insights/page.tsx
+++ apps/web/src/app/(authenticated)/insights/page.tsx
@@ -1,64 +1,42 @@
-import { Suspense, type ReactNode } from 'react';
-import { Flex } from '@mantine/core';
-import { urqlClientSdk } from '@/lib/urql/urql-client';
-import {
-  type DeviceEnum,
-  type InsightsColumnsGroupBy,
-  InsightsColumnsOrderBy,
-  InsightsInterval,
-  type InsightsPosition,
-  OrderBy,
-  type PublisherEnum,
-} from '@/graphql/generated/schema-server';
+'use client';
+
+import { type ReactNode, Suspense, useEffect, useState } from 'react';
+import { Box, Flex, LoadingOverlay } from '@mantine/core';
+import { useAtom } from 'jotai';
+import { InsightsColumnsOrderBy, type InsightsQuery, OrderBy } from '@/graphql/generated/schema-server';
 import LoaderCentered from '@/components/misc/loader-centered';
+import { isInsightsLoadingAtom } from '@/app/atoms/loading-atoms';
+import getInsights, { type SearchParams } from '@/app/(authenticated)/insights/actions';
 import InsightsGrid from './components/insights-grid';
 import OrderFilters from './components/order-filters';
 import PageControls from './components/page-controls';
-
-export interface SearchParams {
-  orderBy?: InsightsColumnsOrderBy;
-  order?: OrderBy;
-  page?: string;
-  pageSize?: string;
-  groupedBy?: InsightsColumnsGroupBy[];
-  account?: string;
-  adId?: string;
-  device?: DeviceEnum;
-  publisher?: PublisherEnum;
-  position?: InsightsPosition;
-  interval?: InsightsInterval;
-  fetchPreviews?: string;
-}
 
 interface InsightsProps {
   searchParams: SearchParams;
 }
 
-export default async function Insights({ searchParams }: InsightsProps): Promise<ReactNode> {
+export default function Insights({ searchParams }: InsightsProps): ReactNode {
   const orderBy = searchParams.orderBy ?? InsightsColumnsOrderBy.spend_rel;
   const order = searchParams.order ?? OrderBy.desc;
   const pageSize = parseInt(searchParams.pageSize ?? '12', 10);
   const page = parseInt(searchParams.page ?? '1', 10);
+  const [isLoading, setIsLoading] = useAtom(isInsightsLoadingAtom);
+  const [insights, setInsights] = useState<InsightsQuery['insights']['edges']>([]);
+  const [hasNextPage, setHasNextPage] = useState<boolean>(false);
 
-  const resp = await urqlClientSdk().insights({
-    adAccountIds: searchParams.account,
-    adIds: searchParams.adId,
-    devices: searchParams.device,
-    groupBy: searchParams.groupedBy,
-    order,
-    orderBy,
-    page,
-    pageSize,
-    positions: searchParams.position,
-    publishers: searchParams.publisher,
-    interval: InsightsInterval.week,
-    fetchPreviews: Boolean(searchParams.fetchPreviews),
-  });
-  const insights = resp.insights.edges;
-  const hasNextPage = resp.insights.hasNext;
+  useEffect(() => {
+    setIsLoading(true);
+    void getInsights(searchParams, orderBy, order, pageSize, page).then((res) => {
+      setInsights(res.insights.edges);
+      setHasNextPage(res.insights.hasNext);
+      setIsLoading(false);
+    });
+  }, [order, orderBy, page, pageSize, searchParams, setIsLoading]);
 
   return (
-    <>
+    <Box pos="relative">
+      <LoadingOverlay visible={isLoading} zIndex={1000} overlayProps={{ radius: 'sm', blur: 2 }} />
+
       <OrderFilters />
       <Suspense fallback={<LoaderCentered type="dots" />}>
         <Flex direction="column">
@@ -66,6 +44,6 @@ export default async function Insights({ searchParams }: InsightsProps): Promise
           <PageControls hasNextPage={hasNextPage} />
         </Flex>
       </Suspense>
-    </>
+    </Box>
   );
 }
