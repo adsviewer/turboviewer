c729c5c9b1b3a5797ab5e6419d65df8cbaef0982
elevchyt <lehy64@gmail.com>
2024-07-06 16:09:05 +0300

    removed loading overlay

--- apps/web/src/app/(authenticated)/insights/page.tsx
+++ apps/web/src/app/(authenticated)/insights/page.tsx
@@ -1,11 +1,9 @@
 'use client';
 
 import { type ReactNode, Suspense, useEffect, useState } from 'react';
-import { Box, Flex, LoadingOverlay } from '@mantine/core';
-import { useAtom } from 'jotai';
+import { Box, Flex } from '@mantine/core';
 import { InsightsColumnsOrderBy, type InsightsQuery, OrderBy } from '@/graphql/generated/schema-server';
 import LoaderCentered from '@/components/misc/loader-centered';
-import { isInsightsLoadingAtom } from '@/app/atoms/loading-atoms';
 import getInsights, { type SearchParams } from '@/app/(authenticated)/insights/actions';
 import InsightsGrid from './components/insights-grid';
 import OrderFilters from './components/order-filters';
@@ -20,23 +18,18 @@ export default function Insights({ searchParams }: InsightsProps): ReactNode {
   const order = searchParams.order ?? OrderBy.desc;
   const pageSize = parseInt(searchParams.pageSize ?? '12', 10);
   const page = parseInt(searchParams.page ?? '1', 10);
-  const [isLoading, setIsLoading] = useAtom(isInsightsLoadingAtom);
   const [insights, setInsights] = useState<InsightsQuery['insights']['edges']>([]);
   const [hasNextPage, setHasNextPage] = useState<boolean>(false);
 
   useEffect(() => {
-    setIsLoading(true);
     void getInsights(searchParams, orderBy, order, pageSize, page).then((res) => {
       setInsights(res.insights.edges);
       setHasNextPage(res.insights.hasNext);
-      setIsLoading(false);
     });
-  }, [order, orderBy, page, pageSize, searchParams, setIsLoading]);
+  }, [order, orderBy, page, pageSize, searchParams]);
 
   return (
     <Box pos="relative">
-      <LoadingOverlay visible={isLoading} zIndex={1000} overlayProps={{ radius: 'sm', blur: 2 }} />
-
       <OrderFilters />
       <Suspense fallback={<LoaderCentered type="dots" />}>
         <Flex direction="column">
--- apps/web/src/app/atoms/loading-atoms.ts
+++
@@ -1,3 +0,0 @@
-import { atom } from 'jotai';
-
-export const isInsightsLoadingAtom = atom<boolean>(false);
