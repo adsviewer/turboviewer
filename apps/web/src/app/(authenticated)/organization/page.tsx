e2672d110ecc3ba86967a6797e6ce41c2d84bbff
elevchyt <lehy64@gmail.com>
2024-07-09 18:22:11 +0300

    org page load data properly

--- apps/web/src/app/(authenticated)/organization/page.tsx
+++ apps/web/src/app/(authenticated)/organization/page.tsx
@@ -1,15 +1,28 @@
+'use client';
+
 import React from 'react';
 import { Flex } from '@mantine/core';
+import { useAtomValue } from 'jotai/index';
+import _ from 'lodash';
+import { initialUserDetails, userDetailsAtom } from '@/app/atoms/user-atoms';
 import DeleteOrganizationButton from '@/app/(authenticated)/organization/components/delete-organization-button';
+import LoaderCentered from '@/components/misc/loader-centered';
 import NameEdit from './components/name-edit';
 // import { UsersTable } from './components/users-table';
 
 export default function Organization(): React.ReactNode {
+  const userDetails = useAtomValue(userDetailsAtom);
   return (
     <Flex direction="column" gap="md" w="100%">
-      <NameEdit />
-      {/*<UsersTable members={members} />*/}
-      <DeleteOrganizationButton />
+      {!_.isEqual(userDetails, initialUserDetails) ? (
+        <>
+          <NameEdit />
+          {/*<UsersTable members={members} />*/}
+          <DeleteOrganizationButton />
+        </>
+      ) : (
+        <LoaderCentered />
+      )}
     </Flex>
   );
 }
