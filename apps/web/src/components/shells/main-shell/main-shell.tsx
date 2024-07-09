68a289d35a479a3bb85235b602f2dbcf78886dad
elevchyt <lehy64@gmail.com>
2024-07-09 14:59:45 +0300

    org select hide if user has no orgs

--- apps/web/src/components/shells/main-shell/main-shell.tsx
+++ apps/web/src/components/shells/main-shell/main-shell.tsx
@@ -65,7 +65,7 @@ export function MainAppShell({ children }: { children: React.ReactNode }): React
             <LogoFull />
           </Flex>
           <Flex align="center" justify="flex-end" ml="auto" gap="sm">
-            <OrganizationSelect />
+            {userDetails.organizations.length ? <OrganizationSelect /> : null}
             <CreateOrganizationButton />
             <SettingsButton />
           </Flex>
