ea8b395cacb0becb38d0c00c76b9b1ecaebec166
Eleftherios Hytiroglou <50188474+elevchyt@users.noreply.github.com>
2024-07-11 01:07:47 -0700

    new nextjs error page (#217)
    * new nextjs error page
    
    * removed unused stuff
    
    * translation fixes

---
+++ apps/web/src/app/error.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import ServerErrorPage from '@/components/misc/server-error-page/server-error-page';
+
+export default function ErrorLayout(): React.ReactNode {
+  return <ServerErrorPage />;
+}
