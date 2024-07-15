397667b3ce93176286754e390b471f8ccb31c5aa
elevchyt <lehy64@gmail.com>
2024-07-15 18:30:43 +0300

    organization parent ID fix

--- apps/server/src/contexts/organization.ts
+++ apps/server/src/contexts/organization.ts
@@ -3,7 +3,7 @@ import { prisma } from '@repo/database';
 export const getRootOrganizationId = async (organizationId: string): Promise<string> => {
   const organization = await prisma.organization.findUniqueOrThrow({ where: { id: organizationId } });
   if (organization.parentId) {
-    return await getRootOrganizationId(organizationId);
+    return await getRootOrganizationId(organization.parentId);
   }
   return organization.id;
 };
