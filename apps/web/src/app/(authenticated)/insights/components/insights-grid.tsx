565e57e6bcdf974251c116cb468f04c993766766
elevchyt <lehy64@gmail.com>
2024-07-17 14:51:35 +0300

    improved error handling & no results found show when appropriate

--- apps/web/src/app/(authenticated)/insights/components/insights-grid.tsx
+++ apps/web/src/app/(authenticated)/insights/components/insights-grid.tsx
@@ -1,50 +1,44 @@
 'use client';
 
-import { Text, SimpleGrid, Transition } from '@mantine/core';
-import { useEffect, useState, type Key, type ReactNode } from 'react';
+import { Text, SimpleGrid } from '@mantine/core';
+import { type Key, type ReactNode } from 'react';
 import { useTranslations } from 'next-intl';
 import { type InsightsQuery } from '@/graphql/generated/schema-server';
+import LoaderCentered from '@/components/misc/loader-centered';
 import InsightCard from './insight-card';
 
 interface PropsType {
   insights: InsightsQuery['insights']['edges'];
+  isDataLoaded: boolean;
 }
 
 export default function InsightsGrid(props: PropsType): ReactNode {
   const t = useTranslations('insights');
-  const [isMounted, setIsMounted] = useState<boolean>(false);
-
-  // Refresh animation on insights data changes
-  useEffect(() => {
-    setIsMounted(false);
-    setTimeout(() => {
-      setIsMounted(true);
-    }, 0);
-  }, [props.insights]);
 
   return (
-    <Transition mounted={isMounted} transition="fade" duration={400} timingFunction="ease">
-      {(styles) => (
-        <div style={styles}>
-          <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }} style={{ display: 'relative' }}>
-            {props.insights.length ? (
-              props.insights.map((insight) => (
-                <InsightCard
-                  key={insight.id as Key}
-                  title={insight.adName ? insight.adName : insight.publisher}
-                  description={insight.position}
-                  device={insight.device}
-                  currency={insight.currency}
-                  datapoints={insight.datapoints}
-                  iframe={insight.iFrame}
-                />
-              ))
-            ) : (
-              <Text>{t('noResultsFound')}</Text>
-            )}
-          </SimpleGrid>
-        </div>
-      )}
-    </Transition>
+    <>
+      {/* Loading */}
+      {!props.isDataLoaded ? <LoaderCentered type="dots" /> : null}
+
+      {/* Empty insights data */}
+      {props.isDataLoaded && !props.insights.length ? <Text>{t('noResultsFound')}</Text> : null}
+
+      {/* Render insights */}
+      <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }} style={{ display: 'relative' }}>
+        {props.insights.length
+          ? props.insights.map((insight) => (
+              <InsightCard
+                key={insight.id as Key}
+                title={insight.adName ? insight.adName : insight.publisher}
+                description={insight.position}
+                device={insight.device}
+                currency={insight.currency}
+                datapoints={insight.datapoints}
+                iframe={insight.iFrame}
+              />
+            ))
+          : null}
+      </SimpleGrid>
+    </>
   );
 }
