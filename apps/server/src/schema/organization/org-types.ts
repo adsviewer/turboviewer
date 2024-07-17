9ad7f7cf2e81a10b142fb150425a5bb6cad5f4bc
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-07-17 14:51:37 +0300

    fix organizations auth scopes

--- apps/server/src/schema/organization/org-types.ts
+++ apps/server/src/schema/organization/org-types.ts
@@ -2,11 +2,14 @@ import { EmailType, OrganizationRoleEnum, UserOrganizationStatus } from '@repo/d
 import { builder } from '../builder';
 
 export const OrganizationDto = builder.prismaObject('Organization', {
-  authScopes: (_organization, _ctx) => {
-    // if (ctx.organizationId && ctx.organizationId === organization.id) return true;
-    // if (ctx.isAdmin) return true;
-    // if (!ctx.organizationId) return false;
-    return true;
+  authScopes: (organization, ctx) => {
+    if (ctx.organizationId && ctx.organizationId === organization.id) return true;
+    if (ctx.isAdmin) return true;
+    if (!ctx.organizationId)
+      return {
+        $granted: 'userOrganization',
+      };
+    return false;
   },
   fields: (t) => ({
     id: t.exposeID('id'),
@@ -55,7 +58,7 @@ export const UserOrganizationDto = builder.prismaObject('UserOrganization', {
     role: t.expose('role', { type: OrganizationRoleEnumDto }),
     status: t.expose('status', { type: UserOrganizationStatusDto }),
     user: t.relation('user'),
-    organization: t.relation('organization'),
+    organization: t.relation('organization', { grantScopes: ['userOrganization'] }),
   }),
 });
 
