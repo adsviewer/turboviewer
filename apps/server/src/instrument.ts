6439e81258613901ee0f86d639a995914bebb4a7
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-07-18 13:14:50 +0300

    hardcode environment

--- apps/server/src/instrument.ts
+++ apps/server/src/instrument.ts
@@ -1,10 +1,10 @@
 import * as Sentry from '@sentry/node';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
-import { MODE } from '@repo/utils';
+import { Environment } from '@repo/utils';
 
 Sentry.init({
   dsn: 'https://b3719092a80366bd037eb7b5adfe15ff@o4507502891040768.ingest.de.sentry.io/4507502899560528',
-  environment: MODE,
+  environment: Environment.Production,
   integrations: [nodeProfilingIntegration(), Sentry.prismaIntegration()],
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
