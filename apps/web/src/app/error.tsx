75ceb28bbb677bb40b23d475e8d5767a784b711b
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-07-19 13:04:37 +0300

    add sourcemaps in sentry (#256)
    * add server sourcemaps
    
    * add channel-ingress sourcemaps
    
    * capture error in sentry in web

--- apps/web/src/app/error.tsx
+++ apps/web/src/app/error.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import * as Sentry from '@sentry/nextjs';
+import { useEffect } from 'react';
 import ServerErrorPage from '@/components/misc/server-error-page/server-error-page';
 
-export default function ErrorLayout(): React.ReactNode {
+export default function ErrorLayout({ error }: { error: unknown }): React.ReactNode {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
   return <ServerErrorPage />;
 }
