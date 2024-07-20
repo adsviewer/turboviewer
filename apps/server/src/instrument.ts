05d7b4dff36d774a015cf09b8e3e79d02e45236d
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-07-20 19:47:32 +0300

    add tracePropagationTargets in server sentry

--- apps/server/src/instrument.ts
+++ apps/server/src/instrument.ts
@@ -8,7 +8,7 @@ Sentry.init({
   integrations: [nodeProfilingIntegration(), Sentry.prismaIntegration()],
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
-
+  tracePropagationTargets: [RegExp(/^http:\/\/api\.adsviewer\.io\/+.$/)],
   // Set sampling rate for profiling - this is relative to tracesSampleRate
   profilesSampleRate: 1.0,
 });
