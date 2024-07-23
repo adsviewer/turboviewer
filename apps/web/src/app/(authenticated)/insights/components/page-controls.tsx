a5bca514958506eb7034b58dd0700867ac58e546
elevchyt <lehy64@gmail.com>
2024-07-23 11:49:09 +0300

    has next page controls improvement

--- apps/web/src/app/(authenticated)/insights/components/page-controls.tsx
+++ apps/web/src/app/(authenticated)/insights/components/page-controls.tsx
@@ -5,18 +5,25 @@ import { IconArrowLeft, IconArrowRight } from '@tabler/icons-react';
 import { useTranslations } from 'next-intl';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
-import { useAtomValue } from 'jotai/index';
+import { useSetAtom, useAtom } from 'jotai/index';
 import { addOrReplaceURLParams, pageKey } from '@/util/url-query-utils';
-import { hasNextInsightsPageAtom } from '@/app/atoms/insights-atoms';
+import { hasNextInsightsPageAtom, insightsAtom } from '@/app/atoms/insights-atoms';
 
 export default function PageControls(): React.ReactNode {
   const t = useTranslations('insights');
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const router = useRouter();
-  const hasNextInsightsPage = useAtomValue(hasNextInsightsPageAtom);
+  const setInsights = useSetAtom(insightsAtom);
+  const [hasNextInsightsPage, setHasNextInsightsPageAtom] = useAtom(hasNextInsightsPageAtom);
+
+  const resetInsights = (): void => {
+    setInsights([]);
+    setHasNextInsightsPageAtom(false);
+  };
 
   const handlePageChange = (type: 'next' | 'prev'): void => {
+    resetInsights();
     if (type === 'next') {
       const nextPage = String(Number(searchParams.get(pageKey) ?? '1') + 1);
       router.replace(addOrReplaceURLParams(pathname, searchParams, pageKey, nextPage));
