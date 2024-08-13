55d55653844924729e8ce188999aae6b8ab66d16
Eleftherios Hytiroglou <50188474+elevchyt@users.noreply.github.com>
2024-08-13 03:37:50 -0700

    logo fix in unauth screens (#337)

--- apps/web/src/app/(unauthenticated)/layout.tsx
+++ apps/web/src/app/(unauthenticated)/layout.tsx
@@ -1,6 +1,15 @@
 import { Suspense } from 'react';
+import { Flex } from '@mantine/core';
 import LoaderCentered from '@/components/misc/loader-centered';
+import { LogoSimple } from '@/components/misc/logo-simple';
 
 export default function RootLayout({ children }: { children: React.ReactNode }): React.ReactNode {
-  return <Suspense fallback={<LoaderCentered />}>{children}</Suspense>;
+  return (
+    <Suspense fallback={<LoaderCentered />}>
+      <Flex justify="center" mt="xl">
+        <LogoSimple />
+      </Flex>
+      {children}
+    </Suspense>
+  );
 }
