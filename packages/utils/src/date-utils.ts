243432d4cd07c6aef8fd9b8f3ebd7d96814d7417
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-08-14 11:31:11 +0300

    date-utils now obey UTC

--- packages/utils/src/date-utils.ts
+++ packages/utils/src/date-utils.ts
@@ -3,9 +3,9 @@ const xMonths = 3;
 export type IntervalType = 'day' | 'week' | 'month' | 'quarter';
 
 export const extractDate = (date: Date): { year: number; month: string; day: string } => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
-  const day = String(date.getDate()).padStart(2, '0');
+  const year = date.getUTCFullYear();
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+  const day = String(date.getUTCDate()).padStart(2, '0');
   return { year, month, day };
 };
 
@@ -20,7 +20,7 @@ export const formatYYYMMDDDate = (date: Date): string => {
 export const getLastXMonths = (): { until: Date; since: Date } => {
   const today = new Date();
   const tomorrow = new Date(today);
-  tomorrow.setDate(today.getDate() + 1);
+  tomorrow.setUTCDate(today.getUTCDate() + 1);
   const lastXMonths = getBeforeXMonths();
   return {
     since: lastXMonths,
@@ -29,37 +29,36 @@ export const getLastXMonths = (): { until: Date; since: Date } => {
 };
 
 export const getDayPriorTillTomorrow = (date?: Date): { until: Date; since: Date } => {
-  const dayPrior = new Date(date ? date : new Date());
   const tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  dayPrior.setDate(dayPrior.getDate() - 1);
+  tomorrow.setUTCDate(tomorrow.getUTCDate() + 1);
   return {
-    since: dayPrior,
+    since: getYesterday(date),
     until: tomorrow,
   };
 };
 
-export const getEndOfDay = (date?: Date | null): Date => {
-  const endOfDay = new Date(date ?? new Date());
-  endOfDay.setHours(23, 59, 59, 999);
-  return endOfDay;
+export const getTomorrowStartOfDay = (date?: Date | null): Date => {
+  const tomorrow = new Date(date ?? new Date());
+  tomorrow.setUTCDate(tomorrow.getUTCDate() + 1);
+  tomorrow.setUTCHours(0, 0, 0, 0);
+  return tomorrow;
 };
 
 export const getYesterday = (date?: Date): Date => {
   const yesterday = date ? date : new Date();
-  yesterday.setDate(yesterday.getDate() - 1);
-  yesterday.setHours(0, 0, 0, 0);
+  yesterday.setUTCDate(yesterday.getUTCDate() - 1);
+  yesterday.setUTCHours(0, 0, 0, 0);
   return yesterday;
 };
 
 export const getBeforeXMonths = (): Date => {
   const today = new Date();
   const xMonthsAgo = new Date(today);
-  xMonthsAgo.setMonth(today.getMonth() - xMonths);
-  xMonthsAgo.setHours(0, 0, 0, 0);
-  if (xMonthsAgo.getFullYear() === today.getFullYear()) {
-    xMonthsAgo.setMonth(0);
-    xMonthsAgo.setDate(1);
+  xMonthsAgo.setUTCMonth(today.getUTCMonth() - xMonths);
+  xMonthsAgo.setUTCHours(0, 0, 0, 0);
+  if (xMonthsAgo.getUTCFullYear() === today.getUTCFullYear()) {
+    xMonthsAgo.setUTCMonth(0);
+    xMonthsAgo.setUTCDate(1);
   }
   return xMonthsAgo;
 };
@@ -68,19 +67,19 @@ export const addInterval = (date: Date, interval: IntervalType | 'seconds', numO
   const result = new Date(date);
   switch (interval) {
     case 'week':
-      result.setDate(result.getDate() + numOfIntervals * 7);
+      result.setUTCDate(result.getUTCDate() + numOfIntervals * 7);
       break;
     case 'quarter':
-      result.setMonth(result.getMonth() + numOfIntervals * 3);
+      result.setUTCMonth(result.getUTCMonth() + numOfIntervals * 3);
       break;
     case 'month':
-      result.setMonth(result.getMonth() + numOfIntervals);
+      result.setUTCMonth(result.getUTCMonth() + numOfIntervals);
       break;
     case 'day':
-      result.setDate(result.getDate() + numOfIntervals);
+      result.setUTCDate(result.getUTCDate() + numOfIntervals);
       break;
     case 'seconds':
-      result.setSeconds(result.getSeconds() + numOfIntervals);
+      result.setUTCSeconds(result.getUTCSeconds() + numOfIntervals);
       break;
   }
   return result;
