f8633b711a331d5059c21b069e779f89e43bd997
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-08-16 10:25:42 +0300

    Revert "increase app_runner cpu"
    This reverts commit e01dbd27ed25d0599cd07f6decf751cd43289190.

--- infrastructure/modules/service/app_runner.tf
+++ infrastructure/modules/service/app_runner.tf
@@ -12,7 +12,7 @@ resource "aws_apprunner_service" "server" {
     protocol = "HTTP"
   }
   instance_configuration {
-    cpu               = 4096
+    cpu               = 4
     memory            = 4096
     instance_role_arn = aws_iam_role.instance_role.arn
   }
