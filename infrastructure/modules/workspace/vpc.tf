f4a66106146d69990203f478ca70bee6baef4440
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-08-16 11:17:12 +0300

    transition to endpoint_interface security group

--- infrastructure/modules/workspace/vpc.tf
+++ infrastructure/modules/workspace/vpc.tf
@@ -125,71 +125,80 @@ locals {
 
 resource "aws_security_group" "endpoint_interface" {
   name        = "${var.environment}-endpoint-interface"
-  description = "ingress and egress for http(s) to the vpc for interface endpoints"
+  description = "ingress and egress for https to the vpc for interface endpoints"
   vpc_id      = aws_vpc.vpc.id
   tags        = { Name = "${var.environment}-endpoint-interface" }
 }
 
-resource "aws_security_group_rule" "interface_ingress_all" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = [aws_vpc.vpc.cidr_block]
-  ipv6_cidr_blocks  = [aws_vpc.vpc.ipv6_cidr_block]
+resource "aws_vpc_security_group_ingress_rule" "ingress_https_in_vpc" {
   security_group_id = aws_security_group.endpoint_interface.id
+
+  from_port                    = 443
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.endpoint_interface.id
+  to_port                      = 443
 }
 
-resource "aws_security_group_rule" "interface_egress_all" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-  security_group_id = aws_security_group.endpoint_interface.id
-}
-
-resource "aws_security_group_rule" "interface_http_tcp" {
-  for_each          = toset(["ingress", "egress"])
-  type              = each.key
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = [aws_vpc.vpc.cidr_block]
-  ipv6_cidr_blocks  = [aws_vpc.vpc.ipv6_cidr_block]
-  security_group_id = aws_security_group.endpoint_interface.id
-}
-
-resource "aws_security_group_rule" "interface_http_quic" {
-  for_each          = toset(["ingress", "egress"])
-  type              = each.key
-  from_port         = 80
-  to_port           = 80
-  protocol          = "udp"
-  cidr_blocks       = [aws_vpc.vpc.cidr_block]
-  ipv6_cidr_blocks  = [aws_vpc.vpc.ipv6_cidr_block]
-  security_group_id = aws_security_group.endpoint_interface.id
-}
-
-resource "aws_security_group_rule" "interface_https_tcp" {
-  for_each          = toset(["ingress", "egress"])
-  type              = each.key
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = [aws_vpc.vpc.cidr_block]
-  ipv6_cidr_blocks  = [aws_vpc.vpc.ipv6_cidr_block]
-  security_group_id = aws_security_group.endpoint_interface.id
-}
-
-resource "aws_security_group_rule" "interface_https_quic" {
-  for_each          = toset(["ingress", "egress"])
-  type              = each.key
-  from_port         = 443
-  to_port           = 443
-  protocol          = "udp"
-  cidr_blocks       = [aws_vpc.vpc.cidr_block]
-  ipv6_cidr_blocks  = [aws_vpc.vpc.ipv6_cidr_block]
-  security_group_id = aws_security_group.endpoint_interface.id
-}
+# resource "aws_security_group_rule" "interface_ingress_all" {
+#   type              = "ingress"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = [aws_vpc.vpc.cidr_block]
+#   ipv6_cidr_blocks  = [aws_vpc.vpc.ipv6_cidr_block]
+#   security_group_id = aws_security_group.endpoint_interface.id
+# }
+#
+# resource "aws_security_group_rule" "interface_egress_all" {
+#   type              = "egress"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   ipv6_cidr_blocks  = ["::/0"]
+#   security_group_id = aws_security_group.endpoint_interface.id
+# }
+#
+# resource "aws_security_group_rule" "interface_http_tcp" {
+#   for_each          = toset(["ingress", "egress"])
+#   type              = each.key
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+#   cidr_blocks       = [aws_vpc.vpc.cidr_block]
+#   ipv6_cidr_blocks  = [aws_vpc.vpc.ipv6_cidr_block]
+#   security_group_id = aws_security_group.endpoint_interface.id
+# }
+#
+# resource "aws_security_group_rule" "interface_http_quic" {
+#   for_each          = toset(["ingress", "egress"])
+#   type              = each.key
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "udp"
+#   cidr_blocks       = [aws_vpc.vpc.cidr_block]
+#   ipv6_cidr_blocks  = [aws_vpc.vpc.ipv6_cidr_block]
+#   security_group_id = aws_security_group.endpoint_interface.id
+# }
+#
+# resource "aws_security_group_rule" "interface_https_tcp" {
+#   for_each          = toset(["ingress", "egress"])
+#   type              = each.key
+#   from_port         = 443
+#   to_port           = 443
+#   protocol          = "tcp"
+#   cidr_blocks       = [aws_vpc.vpc.cidr_block]
+#   ipv6_cidr_blocks  = [aws_vpc.vpc.ipv6_cidr_block]
+#   security_group_id = aws_security_group.endpoint_interface.id
+# }
+#
+# resource "aws_security_group_rule" "interface_https_quic" {
+#   for_each          = toset(["ingress", "egress"])
+#   type              = each.key
+#   from_port         = 443
+#   to_port           = 443
+#   protocol          = "udp"
+#   cidr_blocks       = [aws_vpc.vpc.cidr_block]
+#   ipv6_cidr_blocks  = [aws_vpc.vpc.ipv6_cidr_block]
+#   security_group_id = aws_security_group.endpoint_interface.id
+# }
