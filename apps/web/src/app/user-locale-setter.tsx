ae24cd6fc7edbc4eb871ab3d9fd5140054722bfa
Eleftherios Hytiroglou <50188474+elevchyt@users.noreply.github.com>
2024-08-28 14:06:42 +0300

    Update apps/web/src/app/user-locale-setter.tsx
    Co-authored-by: Giorgos Trichopoulos <giorgos@adsviewer.io>

--- apps/web/src/app/user-locale-setter.tsx
+++ apps/web/src/app/user-locale-setter.tsx
@@ -5,7 +5,7 @@ import { setCookie } from '@/app/actions';
 
 export default function UserLocaleSetter(): React.ReactNode {
   useEffect(() => {
-    void setCookie('USER_LOCALE', navigator.language);
+    Cookies.set('USER_LOCALE', navigator.language);
   }, []);
 
   // eslint-disable-next-line react/jsx-no-useless-fragment -- just an empty react node
