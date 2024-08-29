3b282913d9f4ca270d469c403fc19563a9fe974a
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-08-29 16:47:59 +0300

    pass accept-language header to the backend

--- apps/web/src/lib/urql/urql-client.ts
+++ apps/web/src/lib/urql/urql-client.ts
@@ -1,5 +1,5 @@
 import { cacheExchange, type Client, createClient, fetchExchange } from '@urql/core';
-import { cookies } from 'next/headers';
+import { cookies, headers } from 'next/headers';
 // eslint-disable-next-line import/named -- this should work
 import { cache } from 'react';
 import { REFRESH_TOKEN_KEY, TOKEN_KEY } from '@repo/utils';
@@ -9,9 +9,15 @@ import { env } from '@/env.mjs';
 import { makeAuthExchange } from '@/lib/urql/urql-auth';
 
 const makeClient = (refresh?: boolean): Client => {
-  const token = refresh ? cookies().get(REFRESH_TOKEN_KEY)?.value : cookies().get(TOKEN_KEY)?.value;
+  const cookieStore = cookies();
+  const token = refresh ? cookieStore.get(REFRESH_TOKEN_KEY)?.value : cookieStore.get(TOKEN_KEY)?.value;
   return createClient({
     url: env.NEXT_PUBLIC_REAL_GRAPHQL_ENDPOINT,
+    fetchOptions: {
+      headers: {
+        'accept-language': headers().get('accept-language') ?? 'en-US',
+      },
+    },
     exchanges: [cacheExchange, makeAuthExchange(token), fetchExchange],
     // We may need to add this back in if we run into issues with caching
     // fetchOptions: { cache: 'no-store' },
