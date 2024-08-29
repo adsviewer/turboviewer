3e8d86f18abc5a37362eff22f2172cb4df59fbf2
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-08-29 18:17:20 +0300

    remove web-old

--- apps/web-old/src/app/[locale]/(logged-in)/insights/ad-id.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { urqlClientSdk } from '@/lib/urql/urql-client';
-import MultiFilter from '@/components/filters/multi-filter';
-
-export default async function AdId(): Promise<React.ReactElement | null> {
-  const ads = (await urqlClientSdk().lastThreeMonthsAds()).lastThreeMonthsAds;
-  if (ads.length <= 1) return null;
-  return <MultiFilter options={ads.map((a) => ({ value: a.id, label: a.name ?? a.id }))} groupKey="adId" />;
-}
--- apps/web-old/src/app/[locale]/(logged-in)/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function LoggedInPage(): React.ReactElement {
-  redirect('/insights');
-}
