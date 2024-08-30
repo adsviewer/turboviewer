a1fab3c691ab5089e1c68c6917d47d78930bf8d1
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-08-30 15:35:33 +0300

    444-sub-organizations-should-not-be-able-to-create-new-sub-sub-organizations (#474)
    * don't allow in the backend the creation of sub-sub organizations
    
    * disable org creation for non-root org
    
    * disabled instead of hidden
    
    * fix disabled state for the case of having no org
    
    ---------
    
    Co-authored-by: elevchyt <lehy64@gmail.com>

--- apps/web/src/components/buttons/create-organization-button.tsx
+++ apps/web/src/components/buttons/create-organization-button.tsx
@@ -8,11 +8,15 @@ import { type ReactNode } from 'react';
 import { useTranslations } from 'next-intl';
 import { useForm } from '@mantine/form';
 import { logger } from '@repo/logger';
+import { useAtomValue } from 'jotai';
 import { type CreateOrganizationMutationVariables } from '@/graphql/generated/schema-server';
 import { createAndSwitchOrganization } from '@/app/(authenticated)/organization/actions';
+import { userDetailsAtom } from '@/app/atoms/user-atoms';
 
 export default function CreateOrganizationButton(): ReactNode {
   const t = useTranslations('organization');
+  const tGeneric = useTranslations('generic');
+  const userDetails = useAtomValue(userDetailsAtom);
   const [opened, { open, close }] = useDisclosure(false);
   const [isPending, setIsPending] = useState<boolean>(false);
   const form = useForm({
@@ -41,8 +45,20 @@ export default function CreateOrganizationButton(): ReactNode {
   return (
     <Group justify="center">
       {/* Icon Button */}
-      <Tooltip label={t('createOrganization')}>
-        <ActionIcon onClick={open} variant="default" size={35} aria-label="Create Organization">
+      <Tooltip
+        label={
+          !userDetails.currentOrganization?.isRoot && userDetails.currentOrganization !== null
+            ? tGeneric('accessRootOrg')
+            : t('createOrganization')
+        }
+      >
+        <ActionIcon
+          disabled={!userDetails.currentOrganization?.isRoot && userDetails.currentOrganization !== null}
+          onClick={open}
+          variant="default"
+          size={35}
+          aria-label="Create Organization"
+        >
           <IconPlus />
         </ActionIcon>
       </Tooltip>
