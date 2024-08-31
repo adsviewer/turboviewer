0c125a4c461f7dd626bf926f19fd2983b3b190e6
elevchyt <lehy64@gmail.com>
2024-08-31 10:09:39 +0300

    cleanup

--- apps/landing-page/src/components/Header/menu-data.tsx
+++ apps/landing-page/src/components/Header/menu-data.tsx
@@ -14,67 +14,23 @@ const menuData: Menu[] = [
     path: '/#features',
   },
   {
-    id: 2.1,
+    id: 3,
     title: 'Integrations',
     newTab: false,
     path: '/#integration',
   },
   {
-    id: 2.3,
+    id: 4,
     title: 'Pricing',
     newTab: false,
     path: '/#pricing',
   },
-  // {
-  //   id: 3,
-  //   title: 'Pages',
-  //   newTab: false,
-  //   submenu: [
-  //     {
-  //       id: 31,
-  //       title: 'Blog Grid',
-  //       newTab: false,
-  //       path: '/blog',
-  //     },
-  //     {
-  //       id: 33,
-  //       title: 'Sign In',
-  //       newTab: false,
-  //       path: '/auth/signin',
-  //     },
-  //     {
-  //       id: 34,
-  //       title: 'Sign Up',
-  //       newTab: false,
-  //       path: '/auth/signup',
-  //     },
-  //     {
-  //       id: 35,
-  //       title: 'Docs',
-  //       newTab: false,
-  //       path: '/docs',
-  //     },
   {
-    id: 35.1,
+    id: 5,
     title: 'Support',
     newTab: false,
     path: '/#support',
   },
-  //     {
-  //       id: 36,
-  //       title: '404',
-  //       newTab: false,
-  //       path: '/error',
-  //     },
-  //   ],
-  // },
-
-  // {
-  //   id: 4,
-  //   title: 'Support',
-  //   newTab: false,
-  //   path: '/support',
-  // },
 ];
 
 export default menuData;
