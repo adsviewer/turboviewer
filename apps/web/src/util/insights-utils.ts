a4a4d38ac5094f83cb64c51169d81accfeb97fdd
Eleftherios Hytiroglou <50188474+elevchyt@users.noreply.github.com>
2024-08-31 00:41:45 -0700

    device on insight card (#456)
    * device on insight card
    
    * use icon for device in insights card
    
    * mobile web unique icon

--- apps/web/src/util/insights-utils.ts
+++ apps/web/src/util/insights-utils.ts
@@ -5,9 +5,13 @@ import {
   IconBrandMessenger,
   IconBrandMeta,
   IconBrandTiktok,
+  IconDeviceDesktop,
+  IconDeviceMobile,
+  IconDeviceMobileCode,
+  IconDevices,
   IconHelpOctagon,
 } from '@tabler/icons-react';
-import { PublisherEnum } from '@/graphql/generated/schema-server';
+import { DeviceEnum, PublisherEnum } from '@/graphql/generated/schema-server';
 
 export const publisherToIconMap = new Map<PublisherEnum, React.FC>([
   [PublisherEnum.Facebook, IconBrandFacebook],
@@ -19,4 +23,11 @@ export const publisherToIconMap = new Map<PublisherEnum, React.FC>([
   [PublisherEnum.GlobalAppBundle, IconHelpOctagon],
   [PublisherEnum.Pangle, IconHelpOctagon],
   [PublisherEnum.Unknown, IconHelpOctagon],
+]);
+
+export const deviceToIconMap = new Map<DeviceEnum, React.FC>([
+  [DeviceEnum.Desktop, IconDeviceDesktop],
+  [DeviceEnum.MobileApp, IconDeviceMobile],
+  [DeviceEnum.MobileWeb, IconDeviceMobileCode],
+  [DeviceEnum.Unknown, IconDevices],
 ]);
