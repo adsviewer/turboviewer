0dd10d874e4665574b821bed6282679ec9d121de
elevchyt <lehy64@gmail.com>
2024-09-02 14:40:10 +0300

    removed location and phone number info

--- apps/landing-page/src/components/Contact/index.tsx
+++ apps/landing-page/src/components/Contact/index.tsx
@@ -222,22 +222,22 @@ function Contact(): ReactNode {
                 {t('findUs')}
               </h2>
 
-              <div className="5 mb-7">
+              {/* <div className="5 mb-7">
                 <h3 className="mb-4 text-metatitle3 font-medium text-black dark:text-white">{t('ourLocation')}</h3>
                 <p>{t('address')}</p>
-              </div>
+              </div> */}
               <div className="5 mb-7">
                 <h3 className="mb-4 text-metatitle3 font-medium text-black dark:text-white">{t('email')}</h3>
                 <p>
                   <a href="/">hello@adsviewer.io</a>
                 </p>
               </div>
-              <div>
+              {/* <div>
                 <h4 className="mb-4 text-metatitle3 font-medium text-black dark:text-white">Phone Number</h4>
                 <p>
                   <a href="/">+31 652 43 37 49</a>
                 </p>
-              </div>
+              </div> */}
             </motion.div>
           </div>
         </div>
