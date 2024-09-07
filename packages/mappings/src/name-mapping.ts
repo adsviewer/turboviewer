9abec926e311c1aa42c8f39d30f14febfa660d10
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-09-07 13:47:10 +0300

    use only types from package/database

--- packages/mappings/src/name-mapping.ts
+++ packages/mappings/src/name-mapping.ts
@@ -1,17 +1,17 @@
-import { IntegrationTypeEnum, LoginProviderEnum } from '@repo/database';
+import { type IntegrationTypeEnum, type LoginProviderEnum } from '@repo/database';
 import { camelCase } from 'change-case';
 
 const integrationTypeNameMap = new Map<IntegrationTypeEnum, string>([
-  [IntegrationTypeEnum.META, 'Meta'],
-  [IntegrationTypeEnum.TIKTOK, 'TikTok'],
-  [IntegrationTypeEnum.LINKEDIN, 'LinkedIn'],
+  ['META', 'Meta'],
+  ['TIKTOK', 'TikTok'],
+  ['LINKEDIN', 'LinkedIn'],
 ]);
 
 export const getIntegrationTypeName = (type: IntegrationTypeEnum): string => {
   return integrationTypeNameMap.get(type) ?? camelCase(type);
 };
 
-const loginProviderTypeNameMap = new Map<LoginProviderEnum, string>([[LoginProviderEnum.GOOGLE, 'Google']]);
+const loginProviderTypeNameMap = new Map<LoginProviderEnum, string>([['GOOGLE', 'Google']]);
 
 export const getLoginProviderTypeNameMap = (type: LoginProviderEnum): string => {
   return loginProviderTypeNameMap.get(type) ?? camelCase(type);
