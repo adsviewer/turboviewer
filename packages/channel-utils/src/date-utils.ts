6a7c98e5f8c9e7e298d833cb1eef717509525ecf
aaryansinha16 <aaryansinha16@gmail.com>
2024-09-16 18:43:45 +0530

    Added max-recency boundries

--- packages/channel-utils/src/date-utils.ts
+++ packages/channel-utils/src/date-utils.ts
@@ -1,5 +1,6 @@
 import { addInterval, getDayPriorTillTomorrow, getLastXMonths, getTomorrowStartOfDay } from '@repo/utils';
 import { prisma } from '@repo/database';
+import { tierConstraints } from '@repo/mappings';
 
 export const timeRanges = async (initial: boolean, adAccountId: string): Promise<{ since: Date; until: Date }[]> => {
   if (initial) {
@@ -7,18 +8,20 @@ export const timeRanges = async (initial: boolean, adAccountId: string): Promise
     return splitTimeRange(range.since, range.until);
   }
   const latestInsight = await prisma.insight.findFirst({
-    select: { date: true },
+    select: { date: true, adAccount: { select: { organizations: { select: { tier: true } } } } },
     where: { adAccountId },
     orderBy: { date: 'desc' },
   });
-  const furthestDate = addInterval(new Date(), 'day', -maxTimePeriodDays);
+
+  const organizationTier = latestInsight?.adAccount?.organizations[0]?.tier;
+
+  const furthestDate = addInterval(new Date(), 'day', -tierConstraints[organizationTier || 'Launch'].maxRecency);
   const range = latestInsight ? getDayPriorTillTomorrow(latestInsight.date) : getDayPriorTillTomorrow(furthestDate);
-  return splitTimeRange(range.since, range.until);
+  return splitTimeRange(range.since, range.until, tierConstraints[organizationTier || 'Launch'].maxRecency);
 };
 
-const maxTimePeriodDays = 90; // 90 days
-const maxTimePeriod = 1000 * 60 * 60 * 24 * maxTimePeriodDays;
-export const splitTimeRange = (since: Date, until: Date = new Date()): { since: Date; until: Date }[] => {
+export const splitTimeRange = (since: Date, until: Date = new Date(), maxTimePeriodDays?: number): { since: Date; until: Date }[] => {
+  const maxTimePeriod = 1000 * 60 * 60 * 24 * (maxTimePeriodDays || 7); // min recency in days
   if (since.getTime() > new Date().getTime()) return [];
 
   const periods: { since: Date; until: Date }[] = [];
@@ -26,7 +29,7 @@ export const splitTimeRange = (since: Date, until: Date = new Date()): { since: 
   const diff = until.getTime() - since.getTime();
   if (diff < maxTimePeriod) return [{ since, until }];
 
-  const newDateUntil = addInterval(since, 'day', maxTimePeriodDays - 1);
+  const newDateUntil = addInterval(since, 'day', (maxTimePeriodDays || 7) - 1);
   periods.push({ since, until: newDateUntil });
   const newDateSince = getTomorrowStartOfDay(newDateUntil);
   periods.push(...splitTimeRange(newDateSince, until));
--- packages/mappings/src/tier-mapping.ts
+++ packages/mappings/src/tier-mapping.ts
@@ -1,10 +1,10 @@
 import { type Tier } from '@repo/database';
 
-export const tierConstraints: Record<Tier, { maxUsers: number; maxIntegrations: number }> = {
-  Launch: { maxUsers: 1, maxIntegrations: 1 },
-  Build: { maxUsers: 4, maxIntegrations: Infinity },
-  Grow: { maxUsers: 10, maxIntegrations: Infinity },
-  Scale: { maxUsers: 100, maxIntegrations: Infinity },
+export const tierConstraints: Record<Tier, { maxUsers: number; maxIntegrations: number; maxRecency: number }> = {
+  Launch: { maxUsers: 1, maxIntegrations: 1, maxRecency: 7 },
+  Build: { maxUsers: 4, maxIntegrations: Infinity, maxRecency: 90 },
+  Grow: { maxUsers: 10, maxIntegrations: Infinity, maxRecency: 365 },
+  Scale: { maxUsers: 100, maxIntegrations: Infinity, maxRecency: Infinity },
 };
 
 export function canAddUser(tier: Tier, userOrganizationCount: number): boolean {
