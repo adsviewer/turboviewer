68014a2c48631e96b6bc626a6a34a6bd389aa969
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-09-16 17:57:12 +0300

    add Iresha

--- infrastructure/environments/local/local_variables.tf
+++ infrastructure/environments/local/local_variables.tf
@@ -4,7 +4,7 @@ variable "aws_region" {
 
 variable "developers" {
   type        = set(string)
-  default     = ["giorgos", "lefteris", "aaryan", "dennis"]
+  default     = ["giorgos", "lefteris", "aaryan", "dennis", "iresha"]
   description = "List of developers to be added to the team. This should be a list of aws usernames"
 }
 
