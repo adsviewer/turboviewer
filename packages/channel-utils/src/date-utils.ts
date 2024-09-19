d90e775322fa0a0096f9a38324bc4811885c68f9
Aaryan Sinha <aaryansinha16@gmail.com>
2024-09-19 15:24:13 +0530

    getting the highest tier org/refactoring

--- packages/channel-utils/src/date-utils.ts
+++ packages/channel-utils/src/date-utils.ts
@@ -1,27 +1,36 @@
 import { addInterval, getDayPriorTillTomorrow, getLastXMonths, getTomorrowStartOfDay } from '@repo/utils';
-import { prisma } from '@repo/database';
+import { prisma, Tier } from '@repo/database';
 import { tierConstraints } from '@repo/mappings';
 
 export const timeRanges = async (initial: boolean, adAccountId: string): Promise<{ since: Date; until: Date }[]> => {
-  if (initial) {
-    const range = getLastXMonths();
-    return splitTimeRange(range.since, range.until);
-  }
   const latestInsight = await prisma.insight.findFirst({
     select: { date: true, adAccount: { select: { organizations: { select: { tier: true } } } } },
     where: { adAccountId },
     orderBy: { date: 'desc' },
   });
-
-  const organizationTier = latestInsight?.adAccount?.organizations[0]?.tier;
-
-  const furthestDate = addInterval(new Date(), 'day', -tierConstraints[organizationTier || 'Launch'].maxRecency);
+  
+  const organizations = latestInsight?.adAccount?.organizations ?? [];
+  
+  const organizationWithHighestTier = organizations.length > 0
+    ? organizations.sort((a, b) => tierConstraints[b.tier].order - tierConstraints[a.tier].order)[0].tier
+    : undefined;
+  
+  if (initial) {
+    const range = getLastXMonths();
+    
+    if (!organizationWithHighestTier) {
+      return splitTimeRange(tierConstraints[Tier.Launch].maxRecency, range.since, range.until);
+    }
+  
+    return splitTimeRange(tierConstraints[organizationWithHighestTier].maxRecency, range.since, range.until);
+  }
+  const furthestDate = addInterval(new Date(), 'day', -tierConstraints[organizationWithHighestTier ?? Tier.Launch].maxRecency);
   const range = latestInsight ? getDayPriorTillTomorrow(latestInsight.date) : getDayPriorTillTomorrow(furthestDate);
-  return splitTimeRange(range.since, range.until, tierConstraints[organizationTier || 'Launch'].maxRecency);
+  return splitTimeRange(tierConstraints[organizationWithHighestTier ?? Tier.Launch].maxRecency, range.since, range.until);
 };
 
-export const splitTimeRange = (since: Date, until: Date = new Date(), maxTimePeriodDays?: number): { since: Date; until: Date }[] => {
-  const maxTimePeriod = 1000 * 60 * 60 * 24 * (maxTimePeriodDays || 7); // min recency in days
+export const splitTimeRange = (maxTimePeriodDays: number, since: Date, until: Date = new Date()): { since: Date; until: Date }[] => {
+  const maxTimePeriod = 1000 * 60 * 60 * 24 * maxTimePeriodDays;
   if (since.getTime() > new Date().getTime()) return [];
 
   const periods: { since: Date; until: Date }[] = [];
@@ -29,9 +38,9 @@ export const splitTimeRange = (since: Date, until: Date = new Date(), maxTimePer
   const diff = until.getTime() - since.getTime();
   if (diff < maxTimePeriod) return [{ since, until }];
 
-  const newDateUntil = addInterval(since, 'day', (maxTimePeriodDays || 7) - 1);
+  const newDateUntil = addInterval(since, 'day', (maxTimePeriodDays) - 1);
   periods.push({ since, until: newDateUntil });
   const newDateSince = getTomorrowStartOfDay(newDateUntil);
-  periods.push(...splitTimeRange(newDateSince, until));
+  periods.push(...splitTimeRange(maxTimePeriod, newDateSince, until));
   return periods;
 };
--- packages/mappings/src/tier-mapping.ts
+++ packages/mappings/src/tier-mapping.ts
@@ -1,10 +1,10 @@
 import { type Tier } from '@repo/database';
 
-export const tierConstraints: Record<Tier, { maxUsers: number; maxIntegrations: number; maxRecency: number }> = {
-  Launch: { maxUsers: 1, maxIntegrations: 1, maxRecency: 7 },
-  Build: { maxUsers: 4, maxIntegrations: Infinity, maxRecency: 90 },
-  Grow: { maxUsers: 10, maxIntegrations: Infinity, maxRecency: 365 },
-  Scale: { maxUsers: 100, maxIntegrations: Infinity, maxRecency: Infinity },
+export const tierConstraints: Record<Tier, { maxUsers: number; maxIntegrations: number; maxRecency: number, order: number }> = {
+  Launch: { maxUsers: 1, maxIntegrations: 1, maxRecency: 7, order: 1 },
+  Build: { maxUsers: 4, maxIntegrations: Infinity, maxRecency: 90, order: 2 },
+  Grow: { maxUsers: 10, maxIntegrations: Infinity, maxRecency: 365, order: 3 },
+  Scale: { maxUsers: 100, maxIntegrations: Infinity, maxRecency: Infinity, order: 4 },
 };
 
 export function canAddUser(tier: Tier, userOrganizationCount: number): boolean {
