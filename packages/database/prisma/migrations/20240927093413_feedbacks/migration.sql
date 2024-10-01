9db71a4a777b9bd4e1e7ca2deb181e64d3a1c180
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-10-01 09:01:12 +0300

    fix feedbacks migration

--- packages/database/prisma/migrations/20240927093413_feedbacks/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateEnum
-CREATE TYPE "FeedbackTypeEnum" AS ENUM ('BUG_REPORT', 'FEATURE_SUGGESTION', 'OTHER');
-
--- Rename userId
-ALTER TABLE "Feedback" RENAME COLUMN "userId" TO "user_id";
--- Rename Table
-ALTER TABLE "Feedback" RENAME TO "feedbacks";
