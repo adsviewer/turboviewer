1afac7c6c0732a2abfe36325e770f2e5a2f88e7a
elevchyt <lehy64@gmail.com>
2024-10-06 15:10:52 +0300

    cookies consent

--- apps/landing-page/src/components/google-analytics.tsx
+++ apps/landing-page/src/components/google-analytics.tsx
@@ -4,6 +4,17 @@ import Script from 'next/script';
 export default function GoogleAnalytics(): React.ReactNode {
   return (
     <>
+      {/* Cookies Consent  */}
+      <Script
+        src="https://cdn.consentmanager.net/delivery/autoblocking/32116fec811f0.js"
+        data-cmp-ab="1"
+        data-cmp-host="b.delivery.consentmanager.net"
+        data-cmp-cdn="cdn.consentmanager.net"
+        data-cmp-codesrc="16"
+        strategy="afterInteractive"
+      />
+
+      {/* Analytics */}
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${String(process.env.NEXT_PUBLIC_MEASUREMENT_ID)}`}
