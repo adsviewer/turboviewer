0cf88eaa474405c7b5f81d824c4d5881b967196a
elevchyt <lehy64@gmail.com>
2024-10-11 17:54:25 +0300

    navigation

--- apps/web/src/app/(authenticated)/introduction/page.tsx
+++ apps/web/src/app/(authenticated)/introduction/page.tsx
@@ -1,10 +1,26 @@
 import { Flex } from '@mantine/core';
 import React from 'react';
+import LottieAnimation from '@/components/misc/lottie-animation';
+import wavingPeopleAnimation from '../../../../public/lotties/waving-people.json';
 import VideoCard from './components/video-card';
 
 export default function Introduction(): React.ReactNode {
   return (
-    <Flex justify="center" align="center">
+    <Flex justify="center" align="center" gap="md">
+      <LottieAnimation
+        animationData={wavingPeopleAnimation}
+        speed={0.5}
+        loop
+        playAnimation
+        customStyles={{
+          position: 'absolute',
+          top: 320,
+          left: 220,
+          zIndex: -9999,
+          width: '35rem',
+          transform: 'rotate(-5deg)',
+        }}
+      />
       <VideoCard />
     </Flex>
   );
