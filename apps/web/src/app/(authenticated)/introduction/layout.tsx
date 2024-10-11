19e9ceaca7d0edd3b73646bd783c29c286f7369d
elevchyt <lehy64@gmail.com>
2024-10-11 17:29:34 +0300

    layout stuff

---
+++ apps/web/src/app/(authenticated)/introduction/layout.tsx
@@ -0,0 +1,15 @@
+import React, { Suspense } from 'react';
+import { Title, Text } from '@mantine/core';
+import { useTranslations } from 'next-intl';
+import LoaderCentered from '@/components/misc/loader-centered';
+
+export default function Layout({ children }: { children: React.ReactNode }): React.ReactNode {
+  const t = useTranslations('introduction');
+  return (
+    <>
+      <Title mb="md">{t('title')}</Title>
+      <Text mb="xl">{t('description')}</Text>
+      <Suspense fallback={<LoaderCentered />}>{children}</Suspense>
+    </>
+  );
+}
