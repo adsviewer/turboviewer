d999cdb053178b8a59879c83e680a3bad0681c1e
Eleftherios Hytiroglou <50188474+elevchyt@users.noreply.github.com>
2024-10-11 16:37:12 +0300

    Update apps/web/src/app/(authenticated)/summary/layout.tsx
    Co-authored-by: Giorgos Trichopoulos <giorgos@adsviewer.io>

--- apps/web/src/app/(authenticated)/summary/layout.tsx
+++ apps/web/src/app/(authenticated)/summary/layout.tsx
@@ -3,7 +3,7 @@ import { useTranslations } from 'next-intl';
 import { Title } from '@mantine/core';
 import LoaderCentered from '@/components/misc/loader-centered';
 
-export default function RootLayout({ children }: { children: React.ReactNode }): React.ReactNode {
+export default function SummaryLayout({ children }: { children: React.ReactNode }): React.ReactNode {
   const t = useTranslations('summary');
 
   return (
