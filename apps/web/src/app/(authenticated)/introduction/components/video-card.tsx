bb87548629a7682cd194d8be44f50b7c88d1fcb7
elevchyt <lehy64@gmail.com>
2024-10-11 17:54:25 +0300

    navigation

--- apps/web/src/app/(authenticated)/introduction/components/video-card.tsx
+++ apps/web/src/app/(authenticated)/introduction/components/video-card.tsx
@@ -1,5 +1,53 @@
+'use client';
+
+import Link from 'next/link';
+import { Button, Card, Flex, Group, Image, Text } from '@mantine/core';
+import { IconBuilding, IconPlugConnected } from '@tabler/icons-react';
 import React from 'react';
 
 export default function VideoCard(): React.ReactNode {
-  return <div>page</div>;
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Card.Section>
+        <Image
+          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+          height={350}
+          alt="Norway"
+        />
+      </Card.Section>
+
+      <Group justify="space-between" mt="md" mb="xs">
+        <Text fw={500}>Welcome to AdsViewer!</Text>
+      </Group>
+
+      <Text size="sm" c="dimmed">
+        In order to use AdsViewer you need to set up your integrations and set up your organization.
+      </Text>
+
+      <Flex gap="sm">
+        <Button
+          component={Link}
+          href="/integrations"
+          leftSection={<IconPlugConnected />}
+          variant="gradient"
+          fullWidth
+          mt="md"
+          radius="md"
+        >
+          Go to Integrations
+        </Button>
+        <Button
+          component={Link}
+          href="/organization"
+          leftSection={<IconBuilding />}
+          variant="gradient"
+          fullWidth
+          mt="md"
+          radius="md"
+        >
+          Go to Organization
+        </Button>
+      </Flex>
+    </Card>
+  );
 }
