9f6a5b625f16e4a2a02d785f4cd9703d899bbe64
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-10-17 18:45:43 +0300

    remove isOrg from deleteSearchQueryString

--- apps/web/src/graphql/generated/schema-server.ts
+++ apps/web/src/graphql/generated/schema-server.ts
@@ -668,7 +668,6 @@ export type MutationDeleteOrganizationArgs = {
 
 export type MutationDeleteSearchQueryStringArgs = {
   id: Scalars['String']['input'];
-  isOrganization: Scalars['Boolean']['input'];
 };
 
 export type MutationEmulateAdminArgs = {
