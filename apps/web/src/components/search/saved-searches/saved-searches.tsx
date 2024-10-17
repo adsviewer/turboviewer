f6672f86271510efd78fd930191a9b2e3a3588d4
elevchyt <lefteris@adsviewer.io>
2024-10-17 16:19:14 +0300

    more layout changes + graphql query for searchQueryStrings

--- apps/web/src/components/search/saved-searches/saved-searches.tsx
+++ apps/web/src/components/search/saved-searches/saved-searches.tsx
@@ -6,7 +6,7 @@ import Delete from './delete';
 export default function SavedSearches(): React.ReactNode {
   return (
     <Flex gap="sm" align="center">
-      <Select description="Saved searches" placeholder="Saved searches" w={200} mb="lg" />
+      <Select description="Saved searches" placeholder="Saved searches" miw={200} maw={450} mb="lg" />
       <Save />
       <Delete />
     </Flex>
