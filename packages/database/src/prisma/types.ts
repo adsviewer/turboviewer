9d6591ee1a8018c3b542715f04a76b2fc974eb3d
Lefteris Hytiroglou <lefteris@adsviewer.io>
2024-11-27 13:12:28 +0200

    prims json types

--- packages/database/src/prisma/types.ts
+++ packages/database/src/prisma/types.ts
@@ -1,7 +1,10 @@
-// declare global {
-//   namespace PrismaJson {
-//     type NotificationExtraData = {
-//       commentMentionCreativeId?: string
-//     }
-//   }
-// }
+/* eslint-disable @typescript-eslint/consistent-type-definitions -- this is fine */
+/* eslint-disable @typescript-eslint/no-namespace -- this is fine */
+export {};
+declare global {
+  namespace PrismaJson {
+    type NotificationExtraData = {
+      commentMentionCreativeId?: string;
+    };
+  }
+}
