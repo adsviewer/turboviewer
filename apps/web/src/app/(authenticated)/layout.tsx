cd16562df77408986b4bdc308de2bf884f36b3c0
Lefteris Hytiroglou <lefteris@adsviewer.io>
2024-12-04 12:26:09 +0200

    subscriptions component moved to auth routes only

--- apps/web/src/app/(authenticated)/layout.tsx
+++ apps/web/src/app/(authenticated)/layout.tsx
@@ -1,10 +1,12 @@
 import { MainAppShell } from '@/components/shells/main-shell/main-shell';
 import SetUserSentry from '@/app/(authenticated)/set-user-sentry';
+import { Subscriptions } from '../subscriptions';
 
 export default function RootLayout({ children }: { children: React.ReactNode }): React.ReactNode {
   return (
     <MainAppShell>
       <SetUserSentry />
+      <Subscriptions />
       {children}
     </MainAppShell>
   );
