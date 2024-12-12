80924660086023a7964ea08cb01016fea6fb81f1
Giorgos Trichopoulos <giorgos@adsviewer.io>
2024-12-12 11:29:25 +0200

    729: introduce pinterest feature flag

--- packages/posthog/src/index.ts
+++ packages/posthog/src/index.ts
@@ -3,5 +3,9 @@ import { env } from './config';
 
 const client = new PostHog(env.POSTHOG_API_KEY, { host: 'https://eu.i.posthog.com' });
 
+export enum FeatureFlags {
+  Pinterest = 'pinterest',
+}
+
 export const isFeatureEnabled = async (key: string, distinctId?: string): Promise<boolean> =>
   Boolean(await client.isFeatureEnabled(key, distinctId ?? ''));
